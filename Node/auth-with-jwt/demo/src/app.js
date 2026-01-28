367517c6de572f0353dbff3a099e9b9b21f84ab4
Jefferson <jeff.mazzotta@hotmail.com>
2026-01-28 19:59:09 -0300

    setup cookie-parser and cors for refresh token cookies

--- Node/auth-with-jwt/demo/src/app.js
+++ Node/auth-with-jwt/demo/src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5500",
+    credentials: true,
+  }),
+);
 app.use(express.json());
+app.use(cookieParser());
 app.use(authRoutes);
 
 export default app;
